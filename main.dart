0faeb54eb79ece66286557917d4932adb8e213cd
Kelvin Lopes <kelvin7android@gmail.com>
2020-10-12 20:33:22 +0000

    Initial commit

---
+++ main.dart
@@ -0,0 +1,55 @@
+import 'dart:io';
+
+void main() {
+  print('Sabores para você: ');
+  print('\n');
+  
+
+  const cookies = ['chocolate', 'morango', 'abacaxi', 'cereja'];
+  const cookiesIllustration = ['🍫', '🍓', '🍍', '🍒'];
+
+  for (var i = 0; i < cookies.length; i++) {
+    print(cookies[i]);
+    print(cookiesIllustration[i]);
+  }
+
+print('\n\n');
+
+print('Faça seu pedido 😊');
+
+  var selectClient = stdin.readLineSync();
+
+ print('\n');
+
+  print('Providênciando seu pedido para o sabor ${selectClient}');
+
+  var searchSolicitation = cookies.contains(selectClient.toLowerCase());
+
+  print('\n');
+
+  if (!searchSolicitation) {
+    print('😔 Humm... Infelizmente não temos esse sabor, tente outro!');
+  } else {
+    print('Otima escolha!');
+  }
+
+ print('\n\n');
+
+  leveaOrNot();
+
+  var optionClient = stdin.readLineSync();
+  
+  leveaOrNot();
+
+  if(optionClient == 'sim' || optionClient == 'Sim') {
+    main();
+  }
+
+  else {
+    print('Agradecemos a preferência! Volte sempre... 😁');
+   } 
+}
+
+void leveaOrNot() {
+  print('Deseja fazer um novo pedido? \n Responda com Sim ou Nao ');
+}
